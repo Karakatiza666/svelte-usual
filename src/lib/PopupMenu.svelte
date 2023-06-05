3aade21b2fa806f0aba16dfb2c334dbcc3f4b88a
George <bulakh.96@gmail.com>
2023-06-06 00:32:34 +0300

    Improve PopupMenu state management

--- src/lib/PopupMenu.svelte
+++ src/lib/PopupMenu.svelte
@@ -10,36 +10,45 @@
    let class_ = ''
    export { class_ as class }
    export let btnClass = ''
-
-   onMount(() => {
-      const handleOutsideClick = (event) => {
-         if (show && !menu.contains(event.target) && !button.contains(event.target)) {
-            show = false
-            dispatch('close')
-         }
+   const handleOutsideClick = (event) => {
+      if (show && !menu.contains(event.target) && !button.contains(event.target)) {
+         show = false
+         dispatch('close')
       }
+   }
 
-      const handleEscape = (event) => {
-         if (show && event.key === 'Escape') {
-            show = false
-            dispatch('close')
-         }
+   const handleEscape = (event) => {
+      if (show && event.key === 'Escape') {
+         show = false
+         dispatch('close')
       }
+   }
 
-      // add events when element is added to the DOM
+   const subscribe = () => {
       document.addEventListener('click', handleOutsideClick, false)
       document.addEventListener('keyup', handleEscape, false)
+   }
 
-      // remove events when element is removed from the DOM
-      return () => {
-         document.removeEventListener('click', handleOutsideClick, false)
-         document.removeEventListener('keyup', handleEscape, false)
-      }
-   })
+   const unsubscribe = () => {
+      document.removeEventListener('click', handleOutsideClick, false)
+      document.removeEventListener('keyup', handleEscape, false)
+   }
+
+   onMount(() => unsubscribe) // remove events when element is removed from the DOM)
+
+   $: {
+      setTimeout(show ? subscribe : unsubscribe)
+   }
 </script>
 
 <!-- In order for popup to not display inline it might need a parent with `relative` class -->
-<button bind:this={button} class={'outline-none ' + btnClass} on:click={() => (show = !show)}>
+<button
+   bind:this={button}
+   class={'outline-none ' + btnClass}
+   on:click={() => {
+      show = !show
+   }}
+>
    <slot />
 </button>
 
